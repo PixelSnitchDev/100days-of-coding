fdcf41241f3b69266822db2bcdc85f8d07136d3b
Tirtha Bhalodi <tirthabhalodi@gmail.com>
2026-07-05 15:17:18 +0530

    day2 - datatype,prompt,alert,confirm

---
+++ task5.js
@@ -0,0 +1,19 @@
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old!
+
+//he function confirm shows a modal window with a question and two buttons: OK and Cancel.
+
+//The result is true if OK is pressed and false otherwise.
+
+//For example:
+
+let isBoss = confirm("Are you the boss?");
+
+alert( isBoss ); // true if OK is pressed
+//in browser
+
+
+let name;
+name = prompt("what is your name?",'name');
+alert(`your name is ${name}`);
