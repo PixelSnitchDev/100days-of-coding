85a1b4d785b4b9c89f252656f0e72163789e8674
Tirtha Bhalodi <tirthabhalodi@gmail.com>
2026-07-05 10:54:10 +0530

    day2 - variables

---
+++ task3.js
@@ -0,0 +1,6 @@
+"use strict";
+let admin;
+let name;
+name = "john";
+admin = name;
+console.log( admin );
---
+++ variables.js
@@ -0,0 +1,31 @@
+/*variables are name storage for data.
+var is used in old scripts
+declaring twice raise error
+there exist so-called pure functional programming languages, such as Haskell, that forbid changing variable values.
+*/
+let message;//making a variable
+message = 'hello';//storing a string
+//we can store and define it in single line multiple line.also multiple variables can be stored
+//small task
+"use strict";
+
+// Your personal info as variables
+let myName = "Tirtha";
+let myAge = 20;
+let myCity = "bengaluru";
+const myLanguage = "JavaScript";
+
+console.log(myName);
+console.log(myAge);
+console.log(myCity);
+console.log(myLanguage);
+
+// Math with variables
+let x = 10;
+let y = 20;
+let sum = x + y;
+console.log(sum);
+
+// Try changing a const - see the error
+// const PI = 3.14;
+// PI = 5; // uncomment this to see error
